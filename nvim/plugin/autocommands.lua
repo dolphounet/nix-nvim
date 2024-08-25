9c8dfcb1317a22241abe083bb975ee066c4eb80d
dolphounet <maxencepj.04@gmail.com>
2024-08-25 16:29:36 +0700

    fixed nvim changing foot cursor

--- nvim/plugin/autocommands.lua
+++ nvim/plugin/autocommands.lua
@@ -146,3 +146,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
 --     end
 --   end,
 -- })
+api.nvim_create_autocmd({'VimLeave'}, {
+  callback = function()
+    vim.o.guicursor = 'a:ver25-blinkwait700-blinkoff400-blinkon250'
+  end,
+})
