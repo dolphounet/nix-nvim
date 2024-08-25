100432e15b1cabb1bf599b4f41b07a325c019203
dolphounet <maxencepj.04@gmail.com>
2024-08-25 16:28:07 +0700

    several changes

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -28,6 +28,8 @@ with final.pkgs.lib; let
   all-plugins = with pkgs.vimPlugins; [
     # plugins from nixpkgs go in here.
     # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vimPlugins
+    nvim-tree-lua
+    tokyonight-nvim
     nvim-treesitter.withAllGrammars
     luasnip # snippets | https://github.com/l3mon4d3/luasnip/
     # nvim-cmp (autocompletion) and extensions
@@ -87,6 +89,8 @@ with final.pkgs.lib; let
     # language servers, etc.
     lua-language-server
     nil # nix LSP
+    ripgrep
+    gcc
   ];
 in {
   # This is the neovim derivation
