8b3bf50e24fc629ebdde808ef5525dff04ebf88d
dolphounet <maxencepj.04@gmail.com>
2024-09-04 11:16:07 +0700

    jdtls

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -3,8 +3,10 @@ if vim.g.did_load_lspconfig_plugin then
 end
 vim.g.did_load_lspconfig_plugin = true
 
-local servers = { 'nil_ls', 'lua_ls', 'clangd', 'pyright', 'tsserver', 'jsonls', 'html', 'cssls', 'glsl_analyzer' }
+local servers =
+{ 'nil_ls', 'lua_ls', 'clangd', 'pyright', 'tsserver', 'jsonls', 'html', 'cssls', 'glsl_analyzer', 'jdtls' }
 local navic = require('nvim-navic')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 for _, lsp in ipairs(servers) do
   require('lspconfig')[lsp].setup {
@@ -13,5 +15,6 @@ for _, lsp in ipairs(servers) do
         navic.attach(client, bufnr)
       end
     end,
+    capabilities = capabilities,
   }
 end
