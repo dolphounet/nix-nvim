3bda609114f95a97ea860f7fe1e55df6dc3dc24e
dolphounet <maxencepj.04@gmail.com>
2024-09-23 13:28:44 +0700

    angularls for flake

--- nvim/ftplugin/typescript.lua
+++ nvim/ftplugin/typescript.lua
@@ -1,1 +1,8 @@
-require('lspconfig').angularls.setup {}
+local cmd = { 'angular-language-server', '--stdio' }
+
+require('lspconfig').angularls.setup {
+  cmd = cmd,
+  on_new_config = function(new_config, new_root_dir)
+    new_config.cmd = cmd
+  end,
+}
