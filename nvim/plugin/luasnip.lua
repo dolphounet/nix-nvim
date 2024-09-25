92d18f71d399fcc9db4e741cde3d8549c16037d8
dolphounet <maxencepj.04@gmail.com>
2024-09-25 13:26:02 +0700

    extend JS and TS with html snippets

--- nvim/plugin/luasnip.lua
+++ nvim/plugin/luasnip.lua
@@ -4,6 +4,9 @@ end
 vim.g.did_load_luasnip_plugin = true
 
 require('luasnip').filetype_extend('javascript', { 'html' })
+require('luasnip').filetype_extend('typescript', { 'html' })
 require('luasnip').filetype_extend('javascriptreact', { 'html' })
 require('luasnip').filetype_extend('typescriptreact', { 'html' })
 require('luasnip.loaders.from_vscode').lazy_load()
+require('luasnip.loaders.from_snipmate').load()
+require('luasnip.loaders.from_lua').load()
