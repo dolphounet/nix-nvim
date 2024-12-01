6e64a50c0cd384d95e0d33c1ad4226e0cf50e3a9
dolphounet <maxencepj.04@gmail.com>
2024-12-01 19:33:09 +0700

    C#

---
+++ nvim/ftplugin/cs.lua
@@ -0,0 +1,12 @@
+local navic = require('nvim-navic')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require('lspconfig').omnisharp.setup {
+  on_attach = function(client, bufnr)
+    if client.server_capabilities.documentSymbolProvider then
+      navic.attach(client, bufnr)
+    end
+  end,
+  capabilities = capabilities,
+}
+vim.cmd('LspStart')
