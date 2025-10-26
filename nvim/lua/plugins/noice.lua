604c06de03adeed7510640489d356fe48827fe0f
dolphounet <maxencepj.04@gmail.com>
2025-10-26 19:17:03 +0100

    fix jdtls spam

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -21,6 +21,29 @@ return {
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = true, -- add a border to hover docs and signature help
       },
+
+      routes = {
+        {
+          filter = {
+            event = 'lsp',
+            kind = 'progress',
+            cond = function(message)
+              local client = vim.tbl_get(message.opts, 'progress', 'client')
+              if client ~= 'jdtls' then
+                return false
+              end
+
+              local content = vim.tbl_get(message.opts, 'progress', 'message')
+              if content == nil then
+                return false
+              end
+
+              return string.find(content, 'Validate') or string.find(content, 'Publish')
+            end,
+          },
+          opts = { skip = true },
+        },
+      },
     }
   end,
 }
